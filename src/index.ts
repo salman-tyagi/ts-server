ad7fdae7f03b9c1c69e3cf0d65e7f21a5feb8577
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-31 09:49:52 +0530

    handle get and post request in express with ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
+import express from 'express';
+import morgan from 'morgan';
+
 import app from './server';
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 import loginRouter from './routes/loginRoutes';
 
 app.use(loginRouter);
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,8 +2,32 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Get request by express');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+      <form method="POST">
+        <div>
+          <label for="email">Email</label>
+          <input id="email" name="email" />
+        </div>
+
+        <div>
+          <label for="password">Password</label>
+          <input id="password" name="password" type="password" />
+        </div>
+
+        <button>Submit</button>
+      </form>
+    `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  res.send(`
+      <div>
+        ${email} ${password}
+      </div>
+    `);
 });
 
 export default router;
