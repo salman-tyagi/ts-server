98741ed9cbb78d86f26752766607c1ec77943e9c
Salman Tyagi <salmantyagi15@gmail.com>
2024-09-01 18:05:26 +0530

    added enum values for methods of router

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import express from 'express';
 
+import Methods from './Methods';
+
 export const router = express.Router();
 
 export function controller(routePrefix: string): Function {
@@ -9,8 +11,14 @@ export function controller(routePrefix: string): Function {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata('path', target.prototype, key);
 
+      const method: Methods = Reflect.getMetadata(
+        'method',
+        target.prototype,
+        key
+      );
+
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
